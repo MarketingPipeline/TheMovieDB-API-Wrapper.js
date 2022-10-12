c481fcfb6cdda24d592ec3f6b8a9b92ecf8d0a97
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-10-11 20:47:41 -0400

    Update themoviedb-api-wrapper.js

--- src/themoviedb-api-wrapper.js
+++ src/themoviedb-api-wrapper.js
@@ -1,36 +1,52 @@
+ import tnp from "https://cdn.skypack.dev/torrent-name-parser@0.6.5";
 
-function test(movie_name) {
+function test(movie_name,  max_actors) {
   if(movie_name !=undefined){
-getDetails(`https://api.themoviedb.org/3/search/movie?api_key=6b4357c41d9c606e4d7ebe2f4a8850ea&language=en-US&query=${movie_name}`).then(function(result) {
-var MovieID = result.results[0].id
+    console.log(tnp(movie_name))
+  let name =  tnp(movie_name).title
+   let year = ``
+  if(tnp(movie_name).year){
+   year = `&year=${tnp(movie_name).year}`
+  }  
+    
+ let FinalEndPoint = `https://api.themoviedb.org/3/search/movie?api_key=6b4357c41d9c606e4d7ebe2f4a8850ea&language=en-US&query=${name}${year}`   
+ let Movie_JSON = []
+    
+getDetails(FinalEndPoint).then(function(result) {
   
-var endpoint1 = `https://api.themoviedb.org/3/movie/${result.results[0].id}?&api_key=6b4357c41d9c606e4d7ebe2f4a8850ea`  
+  if(result.total_results === 0){
+    return console.log("No Results Found")
+  } else{ 
+    
+    var MovieID = result.results[0].id
+  
+var endpoint1 = `https://api.themoviedb.org/3/movie/${MovieID}?&api_key=6b4357c41d9c606e4d7ebe2f4a8850ea`  
  getDetails(endpoint1).then(function(result) {
   
-   
+   Movie_JSON.push(result)
 var posterPaths = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
 var backgroundPaths = "http://image.tmdb.org/t/p/w1280";
-   
-   for (const genre in result.genres){
-      console.log(`${result.genres[genre].name}`)
+ getDetails(`https://api.themoviedb.org/3/movie/${MovieID}/casts?&api_key=6b4357c41d9c606e4d7ebe2f4a8850ea`).then(function(result) {
+    var actor_counter = 0 
+   if (!max_actors){
+     max_actors = 5
    }
-   getDetails(`https://api.themoviedb.org/3/movie/${MovieID}/casts?&api_key=6b4357c41d9c606e4d7ebe2f4a8850ea`).then(function(result) {
-    var actor_count = 0 
-    
-    while (actor_count != 5){
+    while (actor_counter !=  max_actors){
 
 
 
       
-   console.log(result.cast[actor_count].name)
-         console.log(`https://image.tmdb.org/t/p/w500${result.cast[actor_count].profile_path}`)
-  actor_count += 1
+   Movie_JSON.push(result.cast[actor_counter])
+  actor_counter += 1
  
     }  
       
+ 
+     console.log(Movie_JSON)
+   
     });     
 });
-
+  }
 });
 
 
@@ -54,7 +70,7 @@ async function getDetails(endpoint) {
   } else{
     console.log("Error - Query Name Not Provided")
   }
+
 }
 
-
- test("The Matrix")  //
+test("The Matrix", 2)  //
